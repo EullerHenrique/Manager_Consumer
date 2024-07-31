4038369eaa0bcc6031247fe2615b1b3938b8a759
Euller Henrique Bandeira Oliveira <euller.oliveira@arphoenix.com.br>
2024-07-31 08:54:51 -0300

    feat: gerar Exce;

--- apps/colaborador/service/internal/util/excelPdfUtilService.py
+++ apps/colaborador/service/internal/util/excelPdfUtilService.py
@@ -2,22 +2,20 @@
 import io
 import xlsxwriter
 
-def gerarExcel(data):
-    json = data.json()
+def gerarExcel(data, keys):
 
     output = io.BytesIO()
     workbook = xlsxwriter.Workbook(output)
     worksheet = workbook.add_worksheet()
 
     #Colunas
-    keys = [key for key, value in json[0].items() if not isinstance(value, dict)]
     for i in range(len(keys)):
         worksheet.write(0, i, keys[i])
 
-    #Linha
-    for i in range(len(json)):
-        for key, value in json[i].items():
-            if not isinstance(value, dict):
+    #Linhas
+    for i in range(len(data)):
+        for key, value in data[i].items():
+            if key in keys:
                 worksheet.write(i+1, keys.index(key), value)
 
     worksheet.autofit()
--- apps/colaborador/views.py
+++ apps/colaborador/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 import apps.colaborador.service.external.dto.colaborador.colaboradorDtoExternalService as colaboradorDtoExternalService
-import apps.colaborador.service.internal.util.excelUtilService as excelUtilService
+import apps.colaborador.service.internal.util.excelPdfUtilService as excelPdfUtilService
 
 def exibirPaginaIndex(request):
     return render(request, 'index.html')
@@ -13,19 +13,19 @@ def exibirPaginaBuscarColaboradores(request):
    
 def listarColaboradores(request):
     try:
-        request.session['tokenJwt'] = request.POST.get('tokenJwt')
+        request.session['tokenJwt'] = request.POST.get('tokenJwt').replace('"', '')
         data = colaboradorDtoExternalService.listarColaboradores(request)
         return render(request, 'listarColaboradores.html', {'colaboradores': data})
     except Exception as e:
         return HttpResponse(e)
 
 def gerarExcel(request):
-    #try:
-    data = requests.get('https://jsonplaceholder.typicode.com/users')
-    byte = excelUtilService.gerarExcel(data)
+    try:
+        data = colaboradorDtoExternalService.listarColaboradores(request)
+        byte = excelPdfUtilService.gerarExcel(data, ['matricula', 'userName', 'nome', 'email', 'ufTrabalho', 'tipoApuracao', 'tipoVinculo', 'codigoArea', 'codigoEquipe', 'codigoEmpresa'])
     
-    response = HttpResponse(byte, content_type='application/vnd.ms-excel')
-    response['Content-Disposition'] = 'attachment; filename=colaboradores.xlsx'
-    return response
-    #except Exception as e:
-    #    return HttpResponse(e)    
+        response = HttpResponse(byte, content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename=colaboradores.xlsx'
+        return response
+    except Exception as e:
+        return HttpResponse(e)    
